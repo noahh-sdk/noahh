27bd6d39a94985deac6a4f54849d07d39887a3ea
Nipfswd <allaj0412@gmail.com>
2026-02-13 12:16:25 +0100

    working on more filters for index mods

--- cmake/NoahhFile.cmake
+++ cmake/NoahhFile.cmake
@@ -20,7 +20,7 @@ function(create_noahh_file proname)
     message(STATUS "Creating noahh file")
 
     if(NOAHH_CLI STREQUAL "NOAHH_CLI-NOTFOUND")
-        message(WARNING "create_noahh_file called, but Noahh CLI was not found - You will need to manually package the .noahh files")
+        message(WARNING "create_noahh_file called, but noahh CLI was not found - You will need to manually package the .noahh files")
     else()
 
         add_custom_target(${proname}_PACKAGE ALL
@@ -29,23 +29,32 @@ function(create_noahh_file proname)
             VERBATIM USES_TERMINAL
         )
     endif()
-
+    
 endfunction()
 
 function(create_noahh_file_v2 proname)
     message(STATUS "Creating noahh file")
 
+    configure_file(${CMAKE_CURRENT_SOURCE_DIR}/mod.json ${CMAKE_CURRENT_BINARY_DIR}/what.txt)
+    set_target_properties(${proname} PROPERTIES CMAKE_CONFIGURE_DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/mod.json)
+
+    execute_process(
+        COMMAND ${NOAHH_CLI} package get-id ${CMAKE_CURRENT_SOURCE_DIR} --raw
+        OUTPUT_VARIABLE MOD_ID
+    )
+
+    set_target_properties(${proname} PROPERTIES PREFIX "")
+    set_target_properties(${proname} PROPERTIES OUTPUT_NAME ${MOD_ID})
+
     if(NOAHH_CLI STREQUAL "NOAHH_CLI-NOTFOUND")
         message(WARNING "create_noahh_file called, but Noahh CLI was not found - You will need to manually package the .noahh files")
     else()
-
         add_custom_target(${proname}_PACKAGE ALL
-            DEPENDS ${proname}
-            COMMAND ${NOAHH_CLI} package new ${CMAKE_CURRENT_SOURCE_DIR} --out $<TARGET_FILE_DIR:${proname}>/${proname}.noahh --install
+            DEPENDS ${proname} ${CMAKE_CURRENT_SOURCE_DIR}/mod.json
+            COMMAND ${NOAHH_CLI} package new ${CMAKE_CURRENT_SOURCE_DIR} --binary $<TARGET_FILE:${proname}> --output $<TARGET_FILE_DIR:${proname}>/${proname}.noahh --install
             VERBATIM USES_TERMINAL
         )
     endif()
-
 endfunction()
 
 function(package_noahh_resources proname src dest prefix)
